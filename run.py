91da3bc7a8e1f4f58439e164b544ef2321408b7c
Erik Grip <erik.grip@outlook.com>
2021-12-31 12:48:28 +0100

    Make run.py start dashboard app.

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-from src import fetch_data
-from src import format_data
+from fetch_chess_data import run as fetch
+from dashboard import run as dashboard
 
 import argparse
 
@@ -9,5 +9,6 @@ if __name__ == '__main__':
     parser.add_argument('player', help='the name of the player at chess.com')
     parser.add_argument('local_tz', help="the players' local time zone (optional)")
     args = parser.parse_args()
-    fetch_data.main(player=args.player)
-    format_data.main(local_tz=args.local_tz)
+
+    fetch.run(player=args.player, local_tz=args.local_tz)
+    dashboard.run()
